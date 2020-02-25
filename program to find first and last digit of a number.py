06e6f9bb799884a7fff5bb160d0ea396b78d8322
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-02-25 20:36:54 +0530

    Add files via upload

---
+++ program to find first and last digit of a number.py
@@ -0,0 +1,11 @@
+#program to find first and last digit of a number
+
+n=int(input("enter the number:"))
+while n >= 10:  
+        n = n / 10
+        print("First digit of a number:")
+while n<=10:
+        n=n/10
+        print("last digit of a number:")
+
+
