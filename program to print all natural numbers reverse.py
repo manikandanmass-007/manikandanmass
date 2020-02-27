d7339854985f60594ca21fd3ab398b8a022d9e9a
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-02-27 19:59:07 +0530

    Add files via upload

---
+++ program to print all natural numbers reverse.py
@@ -0,0 +1,8 @@
+#program to print all natural numbers in reverse
+
+n=int(input("enter the number:"))
+i=n
+print("Natural numbers from n to 1:")
+for i in range (i=n i>0):
+    print (i, end = '  ')
+    
