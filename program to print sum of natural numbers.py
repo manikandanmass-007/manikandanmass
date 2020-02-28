273990afa74cbaa7780ae8191c784f77d01b2036
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-02-28 19:22:54 +0530

    Delete program to print sum of natural numbers.py

--- program to print sum of natural numbers.py
+++
@@ -1,9 +0,0 @@
-#program to print dsum of natural numbers
-
-num=int(input("Enter any number:"))
-total=0
-for value in range(1, num+1):
-    total=total+value
-
-print("The sum of natural number is: {1} ".format(num, total))     
-    
