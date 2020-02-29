a33cc21265ba8292ca1fb8b6bd8e5e07404543fc
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-02-29 19:19:56 +0530

    Add files via upload

---
+++ Python Programs/Program to find factorial of a number.py
@@ -0,0 +1,10 @@
+N = int(input("Enter a number: "))
+factorial=1
+if N < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif N == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,N + 1):
+       factorial = factorial*i
+   print("The factorial of",N,"is",factorial)
