c2ad0231fa3365134be0238b36a40d43c5eb1c84
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-03-02 20:50:26 +0530

    Add files via upload

---
+++ Python Programs/program to check the year is leap year or not.py
@@ -0,0 +1,12 @@
+#program to check the year is leap year or not
+
+year = int(input("Enter Year: "))
+
+if year % 4 == 0 and year % 100 != 0:
+    print(year, "is a Leap Year")
+elif year % 100 == 0:
+    print(year, "is not a Leap Year")
+elif year % 400 ==0:
+    print(year, "is a Leap Year")
+else:
+    print(year, "is not a Leap Year")
