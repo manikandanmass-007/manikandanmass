d50075032c2410fbb4401b49e9db25b01d05aa36
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-03-03 20:43:44 +0530

    Add files via upload

---
+++ Python Programs/program for comparing two strings.py
@@ -0,0 +1,15 @@
+#program for comparing two strings
+
+string1 = input("Enter the first string: ") 
+print(string1, end = "\n") 
+      
+ 
+string2 = input("Enter the second string: ") 
+print(string2, end ="\n") 
+print("Are both strings same: ", end = " ")  
+  
+if (string1 == string2) :  
+    print("String is same") 
+          
+else :  
+    print("String is not same")
---
+++ Python Programs/program to find the length of the string.py
@@ -0,0 +1,9 @@
+#program to find the length of the string
+
+str = input("Enter a string: ")
+
+#c=count
+c = 0
+for s in str:
+      c = c+1
+print("Length of the input string is:", c)
