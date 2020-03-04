97404dbe461bbf4f56431f5046d221779e3f52bd
Manikandan <61351707+manikandanmass-007@users.noreply.github.com>
2020-03-04 21:17:57 +0530

    Add files via upload

---
+++ Python Programs/Program to check if a string is palindrome or not.py
@@ -0,0 +1,13 @@
+# Program to check if a string is palindrome or not
+
+my_str = input("Enter the palindrome:")
+
+my_str = my_str.casefold()
+
+rev_str = reversed(my_str)
+
+
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")
---
+++ Python Programs/Python Program - Convert Lowercase to Uppercase.py
@@ -0,0 +1,10 @@
+# Python Program to Convert String to Uppercase
+ 
+string = input("Please Enter your Own String : ")
+
+#upper() is used to convert lowercase to uppercase
+
+string1 = string.upper()
+ 
+print("\nOriginal String in Lowercase  =  ", string)
+print("The Given String in Uppercase =  ", string1)
